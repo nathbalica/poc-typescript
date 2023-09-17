76e83082a0c2cd5a7600b27327de534bcbf5d23c
nathbalica <nathbalica@gmail.com>
2023-09-17 10:52:49 -0300

    feat: :sparkles: deploy ajust

--- src/database/db.ts
+++ src/database/db.ts
@@ -19,7 +19,7 @@ const configDatabase: DatabaseConfig = {
   port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
   user: process.env.DB_USER || "postgres",
   password: process.env.DB_PASSWORD || "driven",
-  database: process.env.DB_DATABASE || "driviagens"
+  database: process.env.DB_DATABASE || "movies"
 };
 
 
--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ app.use(json());
 app.use(router);
 app.use(errorHandler);
 
-app.listen(5000, () => {
-  console.log("`Servidor rodando na porta 5000`");
-});
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+	console.log(`Servidor rodando na porta ${port}`)
+})
